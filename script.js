946709712c43a1cee4d10e116fb4520af58c31c2
Raphael de Moura <raphaelmoura.dev@gmail.com>
2025-11-12 22:26:37 -0300

    conserto de bug na foto de perfil

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Os contatos não estão disponiveis. Somente Linkedin")
+const submit = document.getElementById("submit");
+
+submit.onclick = function() { 
+    alert("Os contatos não estão disponiveis. Somente Linkedin")
+};
